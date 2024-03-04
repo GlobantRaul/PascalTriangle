8045d131f3d53c449452e89049f0d8fa4ded6f1e
GlobantRaul <161527910+GlobantRaul@users.noreply.github.com>
2024-03-04 11:41:27 -0600

    Add files via upload

---
+++ PascalTriangle.swift
@@ -0,0 +1,24 @@
+﻿func pascalTriangleComputation(_ rowsOfTriangle: Int) -> [[Int]] {
+        // Some initial conditions with rows < 2
+        guard rowsOfTriangle > 0 else { return [] }
+        guard rowsOfTriangle > 1 else { return [[1]] }
+        
+        // Recursive computation to optimize the calculations
+        var rowTriangle = pascalTriangleComputation(rowsOfTriangle - 1)
+        let previousRow = rowTriangle.last!
+        let newRow = [1] + zip(previousRow, previousRow.dropFirst()).map { $0 + $1 } + [1]
+        rowTriangle.append(newRow)
+        
+        return rowTriangle
+}
+
+func printPascalTriangle(_ triangleNumbers: [[Int]]) {
+        for row in triangleNumbers {
+                print(row.map { String($0) }.joined(separator: " "))
+        }
+}
+
+// Test the function
+let rowsOfTriangle = 51
+let triangleNumbers = pascalTriangleComputation(rowsOfTriangle)
+printPascalTriangle(triangleNumbers)
